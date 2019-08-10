bbd3ca456afba93d6b7ddb783e940fae72d9d44b
saurabh wani <saurabh.wani@veritas.com>
2019-08-11 00:35:53 +0530

    Incident:VRP-third Description:Third commit Fix Details:sdf Reviewed By:sdf Merged latest changes from target branch? (y/n):y Dev Build taken? (y/n):y Executed unit tests? (y/n) If (n), why?:y Any Upgrade impact? (y/n/NA):n

--- www/js/Dashboard/controller/DashboardCtrl.js
+++ www/js/Dashboard/controller/DashboardCtrl.js
@@ -4,9 +4,120 @@ angular
   .module('dashboard.module')
   .controller('DashboardCtrl', function($scope) {
 	  
-	  
+	    var request = new XMLHttpRequest();
+	    request.open("GET", "json/assets.json", false);
+	    request.send(null);
+	    var my_JSON_object = JSON.parse(request.responseText);
+	    
+	    var request1 = new XMLHttpRequest();
+	    request1.open("GET", "json/risks.json", false);
+	    request1.send(null);
+	    var my_JSON_object_risk = JSON.parse(request1.responseText);
+	    
+	    
+	    var rgObj = {};
+	    rgObj['LinuxData'] = my_JSON_object["hosts"]["operatingSystem"]["Linux"];
+	    rgObj['WindowsData'] = my_JSON_object["hosts"]["operatingSystem"]["Windows"];
+	    rgObj['UnknownData'] = my_JSON_object["hosts"]["operatingSystem"]["Unknown"];
+        
+        
+        var riskObj = {};
+        riskObj['recoverability'] = my_JSON_object_risk["meta"]["category"]["risk.recoverability"];
+        riskObj['continuity'] = my_JSON_object_risk["meta"]["category"]["risk.continuity"];
+        riskObj['sla'] = my_JSON_object_risk["meta"]["category"]["risk.sla"];
+        
     console.log('DashboardCtrl');
+ // Build the chart
+    Highcharts.chart('hostsDetails', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Hosts by Platform and OS'
+        },
+        tooltip: {
+            pointFormat: '<b>{point.name}</b>:<br>{point.percentage:.1f} %<br>Hosts: {point.y}'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: false,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>:<br>{point.percentage:.1f} %<br>Hosts: {point.y}'
+                },
+                showInLegend: true
+            }
+        },
+        
+        series: [{
+        	
+            name: 'Hosts',
+            colorByPoint: true,
+            data: [{
+                name: 'Linux',
+                
+                y: rgObj['LinuxData'],
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Windows',
+                y: rgObj['WindowsData']
+            }, {
+                name: 'Unknown',
+                y: rgObj['UnknownData']
+            }]
+        }]
+    });
     
+    
+    
+    Highcharts.chart('riskDetails', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Risk Details'
+        },
+        tooltip: {
+            pointFormat: '<b>{point.name}</b>:<br>{point.percentage:.1f} %<br>Total: {point.y}'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: false,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>:<br>{point.percentage:.1f} %<br>Total: {point.y}'
+                },
+                showInLegend: true
+            }
+        },
+        
+        series: [{
+        	
+           // name: 'Risk Cetegory',
+            colorByPoint: true,
+            data: [{
+                name: 'Recoverability',
+                y: riskObj['recoverability'],
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Continuity',
+                y: riskObj['continuity']
+            }, {
+                name: 'SLA',
+                y: rgObj['sla']
+            }]
+        }]
+    });
     
  
 		
--- www/js/ResiliencyGroups/controller/ResiliencyGroupsCtrl.js
+++ www/js/ResiliencyGroups/controller/ResiliencyGroupsCtrl.js
@@ -12,7 +12,7 @@ angular
     
     var request = new XMLHttpRequest();
     request.open("GET", "json/resiliencyGroups.json", false);
-    request.send(null)
+    request.send(null);
     var my_JSON_object = JSON.parse(request.responseText);
     console.log(my_JSON_object["items"]);
     
